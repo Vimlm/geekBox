04d7be01de68348986dca99794928a7ebdd9859f
Victor Martins <VictorMLMartins@icloud.com>
2023-07-11 18:02:56 -0300

    feat: :sparkles: mudanças login
    realizado mudanças em estrutura html do login e na interação de JS
    
    #21

---
+++ src/js/controller/loginController.js
@@ -0,0 +1,21 @@
+import { botaoRegistreSe } from "../view/loginView.js";
+import { emailLogin, senhaLogin } from "../view/loginView.js";
+import { buscaEmail, buscaSenha } from "../model/usuariosModel.js";
+
+botaoRegistreSe.addEventListener('click', event => {
+  event.preventDefault();
+});
+
+let chancesLogin = 0;
+
+if(chancesLogin == 3) {
+  console.log('Você atingiu o número máximo de tentativas para login, favor recuperar a sua senha.');
+} else {
+  if(buscaEmail(emailLogin.value) && buscaSenha(senhaLogin.value)) {
+    console.log('Entrou no sistema.');
+    chancesLogin = 0;
+  } else {
+    chancesLogin += 1;
+    console.log('Senha e email não conferem.');
+  }
+}
---
+++ src/js/view/loginView.js
@@ -0,0 +1,5 @@
+const emailLogin = document.getElementById('email');
+const senhaLogin = document.getElementById('senha');
+const botaoRegistreSe = document.querySelector('.botaoRegistre');
+
+export { emailLogin, senhaLogin, botaoRegistreSe }
