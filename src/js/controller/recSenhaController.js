bb717f78833038fb6fa13f7b8c2ed1fe51e751a0
Victor Martins <VictorMLMartins@icloud.com>
2023-07-13 07:10:22 -0300

    feat: :sparkles: validação recuperar email
    realizado validaçao para verificar email
    
    #47

--- src/js/controller/recSenhaController.js
+++ src/js/controller/recSenhaController.js
@@ -3,8 +3,8 @@ import { email, recBotao } from '../view/recSenhaView.js';
 recBotao.addEventListener('click', event => {
   event.preventDefault();
   if(email.value != '') {
-    alert(`Email enviado com sucesso, verifique o email ${email.value}`);
+    Swal.fire(`Email enviado com sucesso, verifique o email ${email.value}`);
   } else {
-    alert('Preencha um email');
+    Swal.fire(`Preencha um email.`);
   }
 });
